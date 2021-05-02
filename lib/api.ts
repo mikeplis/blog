3b733e920afd70625c3ae0a3eb40886c6a6769f5
Mike Plis <michael.plis01@gmail.com>
2021-05-02 15:19:44 -0400

    Improved type safety of utility method; Added todo for further improvements

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
+import PostType from '../types/post'
 
 const postsDirectory = join(process.cwd(), '_posts')
 
@@ -8,11 +9,35 @@ export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
 }
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
+/**
+ * TODO: I'd like for this to be more typesafe. Not sure if that logic
+ * goes here or somewhere else, but a lot of it flows through here.
+ *
+ * Mainly, I want to enforce that posts have certain frontmatter.
+ * Our Post type says all fields are required, but we don't get
+ * any TS or build errors if we omit those fields from the frontmatter.
+ *
+ * Can we be more typesafe here? The fields param here is a small
+ * optimization, but we can probaby do without it if it helps us get
+ * the type safety we want.
+ *
+ * I think the main problem is that gray-matter just returns our data
+ * as a generic object. Maybe we can create a wrapper around it that
+ * enforces the types better?
+ *
+ *
+ * Tried using typescript-json-validator to generate a validator from the types
+ * but it didn't work well. The simplest option will probably be to create a
+ * validator (with an external lib like joi or manually) and manually keep
+ * is in sync with the types
+ */
+export function getPostBySlug(slug: string, fields: (keyof PostType)[]) {
   const realSlug = slug.replace(/\.md$/, '')
   const fullPath = join(postsDirectory, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
+
+  console.log(JSON.stringify(data, null, 2))
 
   type Items = {
     [key: string]: string
@@ -37,7 +62,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   return items
 }
 
-export function getAllPosts(fields: string[] = []) {
+export function getAllPosts(fields: (keyof PostType)[] = []) {
   const slugs = getPostSlugs()
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
